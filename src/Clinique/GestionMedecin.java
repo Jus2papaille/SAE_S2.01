b5ae6f6785b3993d9b1bd8859ebcdb25ec9a9c8b
Inescgnt <150788651+Inescgnt@users.noreply.github.com>
2024-04-02 09:47:58 +0200

    Update GestionMedecin.java

--- src/Clinique/GestionMedecin.java
+++ src/Clinique/GestionMedecin.java
@@ -1,4 +1,39 @@
-package Clinique;public class GestionMedecin {
+package Clinique;
+
+public class GestionMedecin {
+    private int RPPS;
+	private String nom, prenom, specialisation ;
+	
+	private Medecin(int RPPS, String nom, String prenom, String specialisation) {
+		this.RPPS = RPPS ;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.specialisation = specialisation;
+	}
+
+	public int getIdMed() {
+		return RPPS;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public String getSpecialisation() {
+		return specialisation;
+	}
+
+	@Override
+	public String toString() {
+		return "Medecin [idMed=" + RPPS + ", nom=" + nom + ", prenom=" + prenom + ", specialisation=" + specialisation
+				+ "]";
+	}
+	
+    
     public static void main(String[] args){
     
     }

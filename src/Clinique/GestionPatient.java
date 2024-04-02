7f830f89bb9ccdaad70631e24f59c28d83c87671
Moshinl <150789060+Moshinl@users.noreply.github.com>
2024-04-02 09:48:10 +0200

    Update GestionPatient.java

--- src/Clinique/GestionPatient.java
+++ src/Clinique/GestionPatient.java
@@ -1,3 +1,35 @@
 public class GestionPatient {
+private int idClient;
+	private String nomClient;
+	private String prenomClient;
+	private int ageClient;
+	
+	public Patients (int idClient,String nomClient,String prenomClient, int ageClient){
+		this.idClient=idClient;
+		this.nomClient=nomClient;
+		this.prenomClient=prenomClient;
+		this.ageClient=ageClient;
+	}
 
+	public int getIdClient() {
+		return idClient;
+	}
+
+	public String getNomClient() {
+		return nomClient;
+	}
+
+	public String getPrenomClient() {
+		return prenomClient;
+	}
+
+	public int getAgeClient() {
+		return ageClient;
+	}
+
+	@Override
+	public String toString() {
+		return "Patients [idClient=" + idClient + ", nomClient=" + nomClient + ", prenomClient=" + prenomClient
+				+ ", ageClient=" + ageClient + "]";
+	}
 }

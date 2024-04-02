cb3ae348fc86b149b738c92e70ef880bbd069011
Jus2papaille <matheo.maroille@gmail.com>
2024-04-02 09:20:13 +0200

    debut du projet2

--- src/GestionPatient.java
+++
@@ -1,5 +0,0 @@
-package PACKAGE_NAME;public class GestionPatient {
-    public static void main(String[] args){
-    
-    }
-}
--- src/Main.java
+++
@@ -1,5 +0,0 @@
-package PACKAGE_NAME;public class Main {
-    public static void main(String[] args){
-    
-    }
-}
